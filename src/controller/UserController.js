95baccf605fbd657f967656a5318501455075455
bibisam06 <147711078+bibisam06@users.noreply.github.com>
2025-03-11 17:41:49 +0900

    fix : env값 안 들어오는 에러 수정 (#4)

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -3,8 +3,8 @@ class UserController {
 
     static async addUserInfo(user){
         let foundName = user.name;
-        let foundUser = await user.find
-        if()
+        let foundUser = await user.find;
+       
 
     }
     
