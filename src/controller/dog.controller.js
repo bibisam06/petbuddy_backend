dfcc83d3c2c24252e0770db073677ff1ab6aab06
hanbee jeong <hb.jade00@gmail.com>
2025-07-09 23:27:36 +0900

    fix : 다른 유저 강아지 수정 못하게 방지

--- src/controller/dog.controller.js
+++ src/controller/dog.controller.js
@@ -4,7 +4,8 @@ import Pet from '../models/pet.model.js';
 import FeedReport from '../models/feed.log.model.js';
 //middle-ware
 import { sendResponse } from '../util/response.util.js';
-import { NoDogError } from '../error/error.handler.js';
+import { DogRegistrationError, InternalServerError, InvalidRequestError, NoDogError } from '../error/error.handler.js';
+import { dogRouter } from '../routes/api/dog.routes.js';
 
 const MAX_DOG_PER_USER = 3;
 
@@ -33,9 +34,7 @@ export const createDog = async (req, res, next) => {
 
     console.log("지금 강아지 ", DogsOwnedByUser.length, "마리입니다!!..");
     if(DogsOwnedByUser.length >= MAX_DOG_PER_USER){
-      const dogError = new Error("강아지는 3마리까지 등록가능합니다");
-      dogError.status = 400;
-      throw dogError;
+      throw new DogRegistrationError();
     }
 
     const newDog = await Pet.create({
@@ -44,6 +43,7 @@ export const createDog = async (req, res, next) => {
     });
 
     console.log(grade);
+
     const foodData = await FeedReport.create({
       pet_id : newDog.pet_id,
       user_id: userId, 
@@ -69,13 +69,13 @@ export const createDog = async (req, res, next) => {
 export const findAllDogs = async (req, res, next) => {
   try {
     const userId = req.user.user_id;
+
     const dogs = await Pet.findAll({
       where: { user_id: userId },
       attributes: { exclude: ['createdAt', 'updatedAt'] },
       order: [['pet_id', 'ASC']]
     });
 
-    console.log(dogs);
     if(!dogs){
       throw new NoDogError();
     }
@@ -95,7 +95,6 @@ export const findAllDogs = async (req, res, next) => {
         dog.dataValues.feed = feedData.food_id;
         dog.dataValues.foodGrade = feedData.food_remain_grade;
       } else {
-        // 원하는 대로 null 처리만
         dog.dataValues.feed = null;
         dog.dataValues.foodGrade = null;
       }
@@ -105,7 +104,7 @@ export const findAllDogs = async (req, res, next) => {
   })
 );
 
-    console.log(results);
+    console.log("results : ", results);
 
     return sendResponse(res, 
       {
@@ -122,19 +121,30 @@ export const findAllDogs = async (req, res, next) => {
 export const deleteGangG = async (req, res, next) => {
   try{
     const selectedDog = req.query.dog;
+    const selectedUser = req.user;
     console.log(req.user);
     const dog = await Pet.findOne({
       where: {
         pet_id: selectedDog,
-        user_id: req.user.user_id 
+        user_id: selectedUser.user_id
       }
     });
 
-    if (!dog) {
-      throw new NoDogError("해당 강아지를 찾을 수 없습니다");
+    if(!dog){
+      throw new Error("사용자에게 해당 강아지가 존재하지 않습니다!");
     }
 
-    await Pet.destroy({ where: { pet_id: dog.pet_id } });
+    const result = await Pet.destroy({
+      where : {
+        pet_id : dog.pet_id
+      }
+    });
+
+    console.log(result);
+
+    if(result == 0){
+      throw new InternalServerError("삭제에 실패했습니다");
+    }
 
     return sendResponse(res, {
       responseCode : 200,
@@ -157,19 +167,24 @@ try{
   if(!dogId){
     return new NoDogError("강아지 아이디가 존재하지 않습니다");
   }
+  const user = req.user;
   console.log(typeof dogId);
   const foundDog = await Pet.findOne({
-    where : { pet_id : dogId }
+    where : { 
+      pet_id : dogId,
+      user_id : user.user_id
+    }
   });
 
   if(!foundDog){
-    throw new NoDogError();
+    throw new NoDogError("사용자에게 해당 강아지가 존재하지 않습니다!");
   }
   const dogData = req.body;
   const result = await Pet.update(dogData, {
     where : { pet_id : dogId }
   });
   
+  console.log(result);
 
   return sendResponse(res, {
     responseCode : 200, 
--- src/error/error.handler.js
+++ src/error/error.handler.js
@@ -8,6 +8,24 @@ function createErrorClass(name, defaultMessage, statusCode) {
     }
     };
 }
+//common, server Error
+export const InternalServerError = createErrorClass('InternalServerError', '서버 내부 오류입니다.', 500);
+export const NotImplementedError = createErrorClass('NotImplementedError', '아직 구현되지 않은 기능입니다.', 501);
+
+//validation
+export const ValidationError = createErrorClass('ValidationError', '입력 값이 유효하지 않습니다.', 400);
+export const MissingFieldError = createErrorClass('MissingFieldError', '필수 입력값이 누락되었습니다.', 400);
+
+export const AlreadyExistsError = createErrorClass('AlreadyExistsError', '이미 존재하는 항목입니다.', 409);
+export const ConflictStateError = createErrorClass('ConflictStateError', '현재 상태에서는 이 작업을 수행할 수 없습니다.', 409);
+
+export const PetNotOwnedError = createErrorClass('PetNotOwnedError', '해당 강아지에 대한 접근 권한이 없습니다.', 403);
+export const FeedNotFoundError = createErrorClass('FeedNotFoundError', '급여 정보가 존재하지 않습니다.', 404);
+export const NotificationNotFoundError = createErrorClass('NotificationNotFoundError', '알림 내역이 존재하지 않습니다.', 404);
+
+export const ExternalApiError = createErrorClass('ExternalApiError', '외부 API 호출 중 오류가 발생했습니다.', 502);
+export const S3UploadError = createErrorClass('S3UploadError', '이미지 업로드에 실패했습니다.', 500);
+
 
 // auth, user error 
 export const NoTokenError = createErrorClass('NoTokenError', '요청에 액세스 토큰이 존재하지 않습니다.', 401);
@@ -18,6 +36,7 @@ export const AlreadyRegisterdError = createErrorClass('AlreadyRegisteredError', 
 
 // dog Error 
 export const NoDogError = createErrorClass('NoDogError', '강아지 정보가 존재하지 않습니다.', 404);
+export const DogRegistrationError = createErrorClass('DogRegistrationError', '강아지는 3마리까지 등록가능합니다', 400);
 
 //food
 export const NoFoodError = createErrorClass('NoFoodError', '사료 정보가 조회되지 않습니다', 404);
