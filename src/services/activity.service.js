3a24145f051a744bcf13e9fb1001378718821e6b
hanbee jeong <hb.jade00@gmail.com>
2025-08-19 17:34:19 +0900

    feat : 활동량 조회 기능 구현 (#66)
    - 토큰 없을 때 연동 요청 에러 발생

--- src/services/activity.service.js
+++ src/services/activity.service.js
@@ -6,15 +6,15 @@ import axios from "axios";
 
 //models
 import Pet from '../models/pet.model.js';
-import User from '../models/user.model.js';
 import UserToken from '../models/user.token.model.js';
 
 //service logic import
 import { getUserCredentials } from "./bark.service.js";
+import { NoDogError } from "../error/error.handler.js";
 
 // Get Users Releated Dogs
 const FITBARK_DOG_INFO = "https://app.fitbark.com/api/v2/dog_relations"
-export const getDogSlug = async(token, petId) => {
+export const getDogSlug = async(token) => {
 try{
 
     const response = await axios.get(
@@ -63,14 +63,9 @@ try{
 //TODO : todkkk todo 
 
 export const getuserToken = async(user_id, pet_id) => {
-try{
-    // const userData = await User.findOne({
-    //     where : { 
-    //         user_id : user_id 
-    //     },
-    //     attributes : [ user_slug]
-    // }); //pet _ slug 찾아보고, 없으면 아래로.. 
 
+    // 조회만 하고 없으면 에러처리 -> 클라이언트 연동 유도 
+try{
     const result = await UserToken.findOne({
         where : {
             user_id : user_id,
@@ -79,7 +74,13 @@ try{
         attributes : ['user_token']
     });
 
-    result.user_token 
+    const user_token = result.user_token;
+
+    if(!user_token){
+        throw new NoDogError('해당 사용자가 핏바크 연동된 상태가 아닙니다. 연동을 먼저 진행시켜주시기 바랍니다.');
+    }
+
+    return user_token;
     
 }catch(error){
     console.error(error.message);
